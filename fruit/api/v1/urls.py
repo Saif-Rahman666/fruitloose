5ce7ce4a1aefcc2d11124c5c93d67ffaae61e9e2
saurav_paul <sauravpaul.sunny@gmail.com>
2023-05-07 22:54:22 +0600

    spoil api created

--- fruit/api/v1/urls.py
+++ fruit/api/v1/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from fruit.api.v1.views import FruitListCreateView
+from fruit.api.v1.views import FruitListCreateView, MakeFruitSpoilView
 
 app_name = "fruit-v1-api"
 
 urlpatterns = [
-    path("list-create/", FruitListCreateView.as_view(), name="fruitListCreate")
+    path("list-create/", FruitListCreateView.as_view(), name="fruitListCreate"),
+    path("make-spoiled/<int:id>/", MakeFruitSpoilView.as_view(), name="makeFruitSpoil"),
 ]
--- fruit/api/v1/views.py
+++ fruit/api/v1/views.py
@@ -1,4 +1,6 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework import status
+from rest_framework.generics import ListCreateAPIView, UpdateAPIView
+from rest_framework.response import Response
 
 import fruit
 from fruit.api.v1.serializers import FruitSerializer
@@ -8,3 +10,22 @@ from fruit.models import FruitModel
 class FruitListCreateView(ListCreateAPIView):
     serializer_class = FruitSerializer
     queryset = FruitModel.objects.exclude(spoiled=True)
+
+
+class MakeFruitSpoilView(UpdateAPIView):
+    queryset = FruitModel.objects.all()
+    serializer_class = FruitSerializer
+    lookup_field = 'id'
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        # Check if the fruit is already spoiled
+        if instance.spoiled:
+            return Response({'detail': 'Fruit is already spoiled.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        instance.spoiled = True
+        instance.save()
+
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
