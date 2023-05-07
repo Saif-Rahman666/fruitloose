3ff62de36c43885aa65a645bff0f16badafa8698
saurav_paul <sauravpaul.sunny@gmail.com>
2023-05-07 22:17:51 +0600

    project initialized

---
+++ frutiloose/settings/__init__.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+import environ
+import os
+
+env = environ.Env(
+    DEBUG=(bool, True)
+)
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
+
+SECRET_KEY = env.get_value('SECRET_KEY')
+ENV_TYPE = env.get_value('ENV_TYPE')
+DEBUG = True
+
+if env.str('ENV_TYPE') == 'STAGING':
+    from .staging import *
+elif env.str('ENV_TYPE') == 'PRODUCTION':
+    from .production import *
+else:
+    from .development import *
